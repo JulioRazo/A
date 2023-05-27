5a348e7d7d5bc5ed246a47ebfc38ba331cfe654a
JulioRazo <134310676+JulioRazo@users.noreply.github.com>
2023-05-26 20:39:11 -0700

    Delete practicas directory

--- practicas/script.js
+++
@@ -1,9 +0,0 @@
-function srcChange(url) { 
-    var oldEmbed = document.getElementById('embed1');
-    var newEmbed = oldEmbed.cloneNode(true);
-    newEmbed.src = url;
-    oldEmbed.parentNode.replaceChild(newEmbed, oldEmbed);
-    //location.reload();
-    //alert(document.getElementById('embed1').src);
-} 
-
